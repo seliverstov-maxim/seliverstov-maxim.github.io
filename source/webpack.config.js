089ca2c0318b7989fc2365833619ddfafc8c6e51
Maxim Seliverstov <maxim.web.developer@gmail.com>
2015-09-26 01:30:30 +0300

    Add minification.

--- source/webpack.config.js
+++ source/webpack.config.js
@@ -1,15 +1,21 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack');
 
 var config = {
   entry: path.resolve(__dirname, 'app/main.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle-[hash].js'
+    filename: 'bundle-[hash].min.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.template.html'
+      template: './index.template.html',
+      minify: true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      mangle: false
     })
   ],
   module: {
