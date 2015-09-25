81231e83518e9f0b063b8b5291e2f3af916f534d
Maxim Seliverstov <maxim.web.developer@gmail.com>
2015-09-26 01:16:16 +0300

    Update project

--- source/webpack.config.js
+++ source/webpack.config.js
@@ -1,10 +1,17 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 var config = {
   entry: path.resolve(__dirname, 'app/main.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle-[hash].js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.template.html'
+    })
+  ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
