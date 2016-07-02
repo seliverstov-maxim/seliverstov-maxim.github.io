e0ba633bff876c4efc4bcb08f76ce0eefdcf9994
Maxim Seliverstov <maxim.web.developer@gmail.com>
2016-07-02 23:32:38 +0400

    Update

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
 $(document).ready(function() {
-
   $("#owl-examples").owlCarousel({
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem: true,
-      navigationText: [
-        "<i class='icon-chevron-left icon-white'><</i>",
-        "<i class='icon-chevron-right icon-white'>></i>"
-      ]
-
-      // "singleItem:true" is a shortcut for:
-      // items : 1,
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
-
+    navigation : true,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem: true,
+    navigationText: [
+      "<span class='glyphicon glyphicon-menu-left' aria-hidden='true'></span>",
+      "<span class='glyphicon glyphicon-menu-right' aria-hidden='true'></span>"
+    ]
   });
-
 });
